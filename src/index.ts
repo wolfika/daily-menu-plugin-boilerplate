3a1861d2ea260f0c91f257b22ac3e9090cd940a7
Mate Farkas <mate.farkas@logmein.com>
2019-10-26 16:57:33 +0200

    use @daily-menu/common as git submodule

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export default function greet(): string {
-  return 'Hello World!';
+import { Plugin as PluginAbstraction } from '../common/src/abstractions/Plugin';
+import { HttpService } from '../common/src/abstractions/HttpService';
+
+export default class PluginBoilerplate implements PluginAbstraction {
+  name: 'My daily-menu plugin';
+
+  constructor(private readonly httpService: HttpService) {}
+
+  async provide(): Promise<string> {
+    return 'Hello World!';
+  }
 }
